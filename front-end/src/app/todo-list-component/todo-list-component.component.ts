bd6db852936a99b383f0282cc69aeaec8c08dd5e
David Le <43181047+ShadowVaddix@users.noreply.github.com>
2020-11-08 22:47:24 -0600

    Add files via upload

---
+++ front-end/src/app/todo-list-component/todo-list-component.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-list-component',
+  templateUrl: './todo-list-component.component.html',
+  styleUrls: ['./todo-list-component.component.css']
+})
+export class TodoListComponentComponent {
+  numberFour = 4
+  starterPokemon = ['Charmander','Squirtle','Bulbasaur', 'Pikachu', 'Eevee','Tododile', 'Torchic', 'Treeko']
+
+
+
+}
