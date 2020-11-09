1cf2e721c53cbd7ae8bf7013f8ba13f0d1d4c752
David Le <43181047+ShadowVaddix@users.noreply.github.com>
2020-11-08 22:41:26 -0600

    Add files via upload

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {NamesComponent} from "./names/names.component";
+import {TodoListComponentComponent} from "./todo-list-component/todo-list-component.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'names-navbar', component: NamesComponent},
+  {path: 'todo-list-component', component:TodoListComponentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
