23b078a71ae86d7b3f1ecf17860608a8e3daa07f
David Le <43181047+ShadowVaddix@users.noreply.github.com>
2020-11-08 22:37:45 -0600

    Add files via upload

--- schema_file.js
+++ schema_file.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-var somethingSchema = new Schema ({
+var nameSchema = new Schema ({
     something_field: {type: String, required: true, unique: true, index: 1}
-}, {collection: 'todo'});
-exports.somethingSchema = somethingSchema;
+}, {collection: 'names'});
+exports.nameSchema = nameSchema;
--- server.js
+++ server.js
@@ -1,46 +1,46 @@
-var express = require('express')
-var app= express()
-var bodyParser = require('body-parser');
-
+var express = require('express');
+var app= express();
 var mongoose = require('mongoose');
 
-var db =mongoose.connect("mongodb://localhost/newDB");
-var schema = require('./schema_file.js').somethingSchema;
-var Dodad = mongoose.model('Dodad',schema);
+var db = mongoose.connect("mongodb://localhost/namesdb");
+var nameSchema = require('./schema_file.js').nameSchema;
+var Names = mongoose.model('Names', nameSchema);
 
 
 
 mongoose.connection.once('open', function() {
     app.use('/',express.query())
-        .use(bodyParser.urlencoded({extended:true}))
-        .use( express.static('./public'));
+    app.use(express.json())
+    app.use( express.static('front-end/dist/front-end/'));
 
     app.delete("/list",function (request, response){
-        Dodad.deleteOne({_id: request.query.id}).exec(function (err){
+        Names.deleteOne({_id: request.query.id}).exec(function (err){
             response.status(200);
             response.send(JSON.stringify({}));
         });
     });
 
-    app.post('/save',function (request, response) {
-        var newitem = new Dodad({
-            something_field: request.body.thing
-        });
-        newitem.save(request.body, function (err,doc){
-            console.log(doc)
-            response.status(200);
-            response.send(JSON.stringify({}));
-        })
-
-    });
-
-    app.get('/list',function (request, response) {
-        var query = Dodad.find();
+    app.get('/names', function (request, response) {
+        var query = Names.find();
         query.exec(function (err,docs){
             response.status(200);
             response.send(JSON.stringify({docs}));
         });
     });
+
+    app.post('/name',function (request, response) {
+        var newName = new Names({
+            name: request.body.name
+        });
+        console.log(newName)
+        newName.save(function (err,doc){
+            console.log(doc)
+            response.status(200);
+            response.send(JSON.stringify(doc));
+        })
+
+    });
+
 
     app.listen(8080, function(){
         console.log('Application is running!');
