66f8f1d0b9d0eeb26b554989465370eac8cb2895
David Le <43181047+ShadowVaddix@users.noreply.github.com>
2020-11-18 21:33:22 -0600

    Add files via upload

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NamesComponent } from './names/names.component';
 import { FormsModule} from "@angular/forms";
 import { HttpClientModule} from "@angular/common/http";
 import {TodoListComponentComponent} from "./todo-list-component/todo-list-component.component";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import {TodoListComponentComponent} from "./todo-list-component/todo-list-compon
         BrowserModule,
         AppRoutingModule,
         FormsModule,
-        HttpClientModule
+        HttpClientModule,
+        NgbModule
     ],
   providers: [],
   bootstrap: [AppComponent]
